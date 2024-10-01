e2856b4e1123fa6ac5007b794732d72d24a512c7
Trong Dat <datnt700@gmail.com>
2024-10-01 17:02:27 +0200

    feat: add emotion

---
+++ src/app/Global.style.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+/** @jsxImportSource @emotion/react */
+import { Global, css } from '@emotion/react';
+export const GlobalStyles = () => (
+  <Global
+    styles={css`
+      @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      html,
+      body {
+        font-family: 'Playfair Display', serif;
+        font-style: normal;
+      }
+    `}
+  />
+);
