cf497bf4af48c125377a0e6861f68a9bbf693e1f
Trong Dat <datnt700@gmail.com>
2024-10-04 21:26:55 +0200

    fix: link to file

--- src/app/_hooks/useCategory.ts
+++ src/app/_hooks/useCategory.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { fetchCategory } from '@/app/_services/categories';
+import { fetchCategory } from '../_services/categories';
 
 export const useCategories = () => {
   return useQuery({
